d47c61e0995b7a0ee8ee3c08209d2da115b25c20
Hudhud571 <kalcefx571@gmail.com>
2025-03-05 17:55:10 -0500

    Add files via upload

---
+++ C#projects/Entity Framework Code-First/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        using (var context = new StudentContext())
+        {
+            var student = new Student { Name = "Abdirahman Said", Age = 30 };
+            context.Students.Add(student);
+            context.SaveChanges();
+            Console.WriteLine("Student added successfully!");
+        }
+    }
+}
---
+++ C#projects/Entity Framework Code-First/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Student
+{
+    [Key]
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
