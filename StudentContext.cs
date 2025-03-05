4e95d092c46cac77ca9a04de1aa878803d89cc26
Hudhud571 <kalcefx571@gmail.com>
2025-03-05 17:57:57 -0500

    Add files via upload

---
+++ StudentContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+public class StudentContext : DbContext
+{
+    public StudentContext() : base("StudentDB")
+    {
+    }
+
+    public DbSet<Student> Students { get; set; }
+}
